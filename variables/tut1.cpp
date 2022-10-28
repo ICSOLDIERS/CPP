94e235975ee32d8cee9c38468fde83508b861e42
ABHISHEK SINGH <81334208+ICSOLDIERS@users.noreply.github.com>
2022-10-28 07:15:32 +0530

    first chapter completed

---
+++ variables/tut1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+// main is a entry point and process start
+{  //function body start
+    std::cout<<"hello world icsoldiers is here"; 
+    //std is namespace
+    // cout for printing
+    return 0;
+}  //function body close
---
+++ variables/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// this programm was created by indian cyber soldiers
+int main(){
+    int sum =6;
+    cout<< "hello world"<< sum;
+    return 0;
+}
+
+/* this 
+is a
+multiline 
+comment*/
